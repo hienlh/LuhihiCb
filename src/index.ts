f3810a7386e8fa10eb29e69d225f119aa6cd8c23
hienlh <hienlh1298@gmail.com>
2019-05-10 00:58:48 +0700

    Change initialize chatbot

--- src/index.ts
+++ src/index.ts
@@ -1,30 +1,10 @@
 import config from './configuration'
-import {FbMessAPI} from './framework/FbMessAPI';
-import {Nested, Postback} from './framework/model';
-import {Postbacks} from './helper/postbacks';
+import {InitializeChatbot} from './initializeChatbot';
 import mainRouter from './router';
 
 const app = config();
 
-
-// FbMessAPI.setGreeting([{locale: 'default', text: 'Chào mừng bạn đến với Luhihi Chatbot!'}]);
-// FbMessAPI.setGetStarted(Postbacks.GetStarted).then(() => console.log('Set Get Started success.'));
-// FbMessAPI.setPersistentMenu([
-//     new Nested('Chức năng chính', [
-//         new Postback('Xem hình', Postbacks.ViewPicture),
-//         new Postback('Thả thính ♥♡♥', Postbacks.NeedLove)
-//     ]),
-//     new Nested('Nhắn tin', [
-//         new Postback('Dừng nhắn tin', Postbacks.CancelChat),
-//         new Postback('Xin facebook', Postbacks.RequestFacebook)
-//     ]),
-//     new Nested('Khác', [
-//         new Postback('Ai thích hình của tôi?', Postbacks.ViewRequests + 0),
-//         new Postback('Xem hình của tôi', Postbacks.ViewMyPicture),
-//         new Postback('Xoá hình của tôi', Postbacks.RemovePicture),
-//         new Postback('Cập nhật profile', Postbacks.UpdateProfile)
-//     ])
-// ]).then(() => console.log('Set persistent menu success'));
+InitializeChatbot();
 
 app.use(mainRouter);
 
